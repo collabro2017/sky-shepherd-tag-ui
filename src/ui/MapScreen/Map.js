3be26e09b9f14884f6d81db9290ec59bfd4ebd0b
Eric Watson <wasnotrice@gmail.com>
2018-01-17 11:40:14 -0600

    Extract area transformation functions

--- src/ui/MapScreen/Map.js
+++ src/ui/MapScreen/Map.js
@@ -4,95 +4,12 @@ import { connect } from "react-redux"
 import { Alert, View } from "react-native"
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps"
 import Polygon from "./Polygon"
+import { coordinatesFromArea, regionFromArea } from "./area"
 import type { Region, State } from "../../state/types"
-import type { Area, Coordinate, Point } from "../../data/types"
+import type { Area, Coordinate } from "../../data/types"
 import styles from "../../styles"
 
 import { mapSelectors, mapOperations } from "../../state/map"
-
-type IndexesAndScale = {
-  index: number,
-  reference: number,
-  scale: number
-}
-
-type PointsAndScale = {
-  point: Point,
-  zeroPoint: Point,
-  scale: number
-}
-
-const maxMinPosRatio = 100000.0
-
-// Convert index to lat/long int
-const indexToPosition = ({
-  index,
-  reference,
-  scale
-}: IndexesAndScale): number => {
-  return ((index * scale) >> 4) + reference
-}
-
-// Convert indexed point to mappable coordinate
-const coordinateFromPoint = ({
-  point,
-  zeroPoint,
-  scale
-}: PointsAndScale): Coordinate => {
-  const x = indexToPosition({
-    index: point.x,
-    reference: zeroPoint.x,
-    scale: scale
-  })
-  const y = indexToPosition({
-    index: point.y,
-    reference: zeroPoint.y,
-    scale: scale
-  })
-
-  return {
-    latitude: y / maxMinPosRatio,
-    longitude: x / maxMinPosRatio
-  }
-}
-
-const coordinatesFromArea = (area: ?Area): Coordinate[] => {
-  if (typeof area === "undefined" || area === null) {
-    return []
-  }
-
-  const { minPos, scale, points } = area
-  return points.pointsArray.map((point: Point): Coordinate => {
-    return coordinateFromPoint({ point, scale, zeroPoint: minPos })
-  })
-}
-
-type MinMax = {
-  min: number,
-  max: number
-}
-
-const calculateDelta = (extent: MinMax): number => {
-  const paddingFactor = 1.1
-  const delta = Math.abs(extent.max - extent.min) / maxMinPosRatio
-  return delta * paddingFactor
-}
-const latitudeDelta = (area: Area): number => {
-  return calculateDelta({ max: area.maxPos.y, min: area.minPos.y })
-}
-
-const longitudeDelta = (area: Area): number => {
-  return calculateDelta({ max: area.maxPos.x, min: area.minPos.x })
-}
-
-const regionFromArea = (area: Area): Region => {
-  return {
-    latitude: area.centroid.latitude,
-    longitude: area.centroid.longitude,
-    latitudeDelta: latitudeDelta(area),
-    longitudeDelta: longitudeDelta(area)
-  }
-}
 
 const mapStateToProps = (state: State, ownProps: Props) => {
   const provider: string = PROVIDER_GOOGLE
@@ -148,7 +65,7 @@ class Map extends Component<Props> {
     const region = pickRegion(this.props)
     const props: Props = { ...this.props, region }
     const { area } = props
-    const coordinates = coordinatesFromArea(area)
+    const coordinates: Coordinate[] = coordinatesFromArea(area)
     return (
       <View style={{ flex: 1 }}>
         <MapView {...props}>
---
+++ src/ui/MapScreen/area.js
@@ -0,0 +1,89 @@
+// @flow
+import type { Area, Coordinate, Point } from "../../data/types"
+import type { Region } from "../../state/types"
+
+type IndexesAndScale = {
+  index: number,
+  reference: number,
+  scale: number
+}
+
+type PointsAndScale = {
+  point: Point,
+  zeroPoint: Point,
+  scale: number
+}
+
+const maxMinPosRatio = 100000.0
+
+// Convert index to lat/long int
+const indexToPosition = ({
+  index,
+  reference,
+  scale
+}: IndexesAndScale): number => {
+  return ((index * scale) >> 4) + reference
+}
+
+// Convert indexed point to mappable coordinate
+const coordinateFromPoint = ({
+  point,
+  zeroPoint,
+  scale
+}: PointsAndScale): Coordinate => {
+  const x = indexToPosition({
+    index: point.x,
+    reference: zeroPoint.x,
+    scale: scale
+  })
+  const y = indexToPosition({
+    index: point.y,
+    reference: zeroPoint.y,
+    scale: scale
+  })
+
+  return {
+    latitude: y / maxMinPosRatio,
+    longitude: x / maxMinPosRatio
+  }
+}
+
+const coordinatesFromArea = (area: ?Area): Coordinate[] => {
+  if (typeof area === "undefined" || area === null) {
+    return []
+  }
+
+  const { minPos, scale, points } = area
+  return points.pointsArray.map((point: Point): Coordinate => {
+    return coordinateFromPoint({ point, scale, zeroPoint: minPos })
+  })
+}
+
+type MinMax = {
+  min: number,
+  max: number
+}
+
+const calculateDelta = (extent: MinMax): number => {
+  const paddingFactor = 1.1
+  const delta = Math.abs(extent.max - extent.min) / maxMinPosRatio
+  return delta * paddingFactor
+}
+const latitudeDelta = (area: Area): number => {
+  return calculateDelta({ max: area.maxPos.y, min: area.minPos.y })
+}
+
+const longitudeDelta = (area: Area): number => {
+  return calculateDelta({ max: area.maxPos.x, min: area.minPos.x })
+}
+
+const regionFromArea = (area: Area): Region => {
+  return {
+    latitude: area.centroid.latitude,
+    longitude: area.centroid.longitude,
+    latitudeDelta: latitudeDelta(area),
+    longitudeDelta: longitudeDelta(area)
+  }
+}
+
+export { coordinatesFromArea, regionFromArea }
