3b55e63db667c6a36eaa75bd0375e12e1d288607
crayfellow <patrick@krekeltronics.com>
2018-02-16 13:22:40 -0600

    Beginnings of an env for basic dev

---
+++ src/data/cloud/env-production.js
@@ -0,0 +1,6 @@
+export const PARSE_APPLICATION_ID = "ROkx97AeQ43jyd5fI25P4bK4mdUr19pH"
+export const PARSE_SERVER_URL = "https://fido-staging.skyshepherd.com/1"
+
+// HACK: Pending proper login
+export const PARSE_EMAIL = "<some email>"
+export const PARSE_PASSWORD = "<some password>"
