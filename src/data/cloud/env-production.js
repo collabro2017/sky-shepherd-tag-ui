878cc5e1fab674e4390d5a574cbfcf179e087532
Eric Watson <wasnotrice@gmail.com>
2018-02-27 17:06:52 -0600

    Add dummy account credentials

--- src/data/cloud/env-production.js
+++ src/data/cloud/env-production.js
@@ -2,5 +2,5 @@ export const PARSE_APPLICATION_ID = "ROkx97AeQ43jyd5fI25P4bK4mdUr19pH"
 export const PARSE_SERVER_URL = "https://fido-staging.skyshepherd.com/1"
 
 // HACK: Pending proper login
-export const PARSE_EMAIL = "<some email>"
-export const PARSE_PASSWORD = "<some password>"
+export const PARSE_EMAIL = "eric+brianblue@citizen.io"
+export const PARSE_PASSWORD = "secret123"
