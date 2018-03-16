b1c5a1e0369da08d4b1eb6314f7b68ebc0b248fc
Eric Watson <wasnotrice@gmail.com>
2018-03-16 15:44:37 -0500

    Use normal account, not admin
    Admin account has too many boundaries. Causes performance issues with
    the current state handling

--- src/cloud/env-staging.js
+++ src/cloud/env-staging.js
@@ -2,7 +2,10 @@ export const PARSE_APPLICATION_ID = "ROkx97AeQ43jyd5fI25P4bK4mdUr19pH"
 export const PARSE_SERVER_URL = "https://fido-staging.skyshepherd.com/1"
 
 // HACK: Pending proper login
-// export const PARSE_EMAIL = "eric+brianblue@citizen.io"
-// export const PARSE_PASSWORD = "secret123"
-export const PARSE_EMAIL = "eric+1@citizen.io"
-export const PARSE_PASSWORD = "calcium-charity-pottery"
+// Normal user
+export const PARSE_EMAIL = "eric+brianblue@citizen.io"
+export const PARSE_PASSWORD = "secret123"
+
+// Admin user, pulls in ~150 boundaries
+// export const PARSE_EMAIL = "eric+1@citizen.io"
+// export const PARSE_PASSWORD = "calcium-charity-pottery"
