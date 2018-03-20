e18a788f59311e85df5f1906a7dca9dd9b1a49d1
Eric Watson <wasnotrice@gmail.com>
2018-03-20 11:04:22 -0500

    WIP: add keys to area changes view

--- src/ui/MapScreen/MapContent/AreaChangesMapContent.js
+++ src/ui/MapScreen/MapContent/AreaChangesMapContent.js
@@ -5,14 +5,27 @@ import Polygon from "./Polygon"
 import AreaCoordinateMarker from "./AreaCoordinateMarker"
 import type { AreaChanges } from "../../../types"
 
+const coordinateKey = ({ latitude, longitude }: Coordinate): string =>
+  "(" + latitude.toString() + "," + longitude.toString() + ")"
+
+const coordinatesKey = (coordinates: Coordinate[]): string =>
+  "[" + coordinates.map(coordinateKey).join(",") + "]"
+
 export default function AreaChangesMapContent({ areaChanges }: Props) {
   // Google Maps crashes if a polygon has no coordinates
   if (areaChanges != null && areaChanges.coordinates.length > 0) {
+    const { coordinates } = areaChanges
     return (
       <View>
-        <Polygon coordinates={areaChanges.coordinates} />
-        {areaChanges.coordinates.map((coordinate, index) => (
-          <AreaCoordinateMarker coordinate={coordinate} key={index} />
+        <Polygon
+          coordinates={coordinates}
+          identifier={coordinatesKey(coordinates)}
+        />
+        {coordinates.map(coordinate => (
+          <AreaCoordinateMarker
+            coordinate={coordinate}
+            key={coordinateKey(coordinate)}
+          />
         ))}
       </View>
     )
